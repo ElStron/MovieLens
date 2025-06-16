9abd13a0c2faad66a324744ceb219ca674552793
ElStron <luisstron45@gmail.com>
2025-06-15 22:49:44 -0600

    Refactor MoviesController to improve type safety and enhance movie retrieval methods

--- src/controllers/MoviesController.php
+++ src/controllers/MoviesController.php
@@ -2,42 +2,54 @@
 namespace App\Controllers;
 
 use App\Core\Controller;
+use App\Core\Database;
 use App\Services\MovieService;
+use App\Repositories\MovieRepository; 
+use App\Models\Movie; 
 
 class MoviesController extends Controller
 {
-    private $movieService;
+    private MovieService $movieService;
+
     public function __construct()
     {
-        $this->movieService = new MovieService();
+
+        $database = new Database(); 
+        $movieRepository = new MovieRepository($database); 
+        $this->movieService = new MovieService($movieRepository);
     }
 
     public function index(): void
     {
-        $movies = $this->movieService->get(10, 0);
-        if (!$movies) {
+        /** @var Movie[] $movies */
+        $movies = $this->movieService->getPaginatedMovies(10, 0);
+
+        if (empty($movies)) {
             http_response_code(404);
             return;
         }
+
         $this->view('movies/index', [
             'title' => 'Lista de Películas',
             'description' => 'Aquí puedes ver todas las películas disponibles.',
-            'movies' => $movies
+            'movies' => $movies 
         ]);
     }
 
     public function show(string $slug): void
     {
-        $movie = $this->movieService->getBySlug($slug);
+        /** @var Movie|null $movie */
+        $movie = $this->movieService->getMovieBySlug($slug);
 
         if (!$movie) {
             http_response_code(404);
+            echo "Alto ahí rufian, no hay nada que ver aquí.";
             return;
         }
 
         $this->view('movies/details', [
-            'title' => $movie['titulo_movie'],
-            'description' => $movie['sinopsis_movie'],
+            'title' => $movie->title,
+            'description' => $movie->synopsis,
             'movie' => $movie
         ]);
     }
