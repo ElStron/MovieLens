91fc4756d65c671a862d74cf86bba09ce91c7b8f
ElStron <luisstron45@gmail.com>
2025-06-15 22:54:47 -0600

    Update renderMovieCard to use object properties for movie data

--- src/ui/components/movieCard.php
+++ src/ui/components/movieCard.php
@@ -2,11 +2,11 @@
 function renderMovieCard($movie) {
     ob_start();
     ?>
-    <article class='movie-card' style="background-image: url('<?= $movie['poster_movie_2'] ?? '' ?>')">
+    <article class='movie-card' style="background-image: url('<?= $movie->posterImage ?? '' ?>')">
         <div class='movie-info'>
-            <h2 class='movie-title'><?= htmlspecialchars($movie['titulo_movie'] ?? '') ?></h2>
-            <p class='movie-description'><?= htmlspecialchars($movie['sinopsis_movie'] ?? '') ?></p>
-            <a href='/pelicula/<?= htmlspecialchars($movie['slug_movie'] ?? '') ?>' class='movie-link'>Ver detalles</a>
+            <h2 class='movie-title'><?= htmlspecialchars($movie->title ?? '') ?></h2>
+            <p class='movie-description'><?= htmlspecialchars($movie->synopsis ?? '') ?></p>
+            <a href='/pelicula/<?= htmlspecialchars($movie->slug ?? '') ?>' class='movie-link'>Ver detalles</a>
         </div>
     </article>
     <?php
