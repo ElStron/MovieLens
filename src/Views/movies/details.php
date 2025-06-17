179e42093ea11916844f336298e536cc3673bfa5
ElStron <luisstron45@gmail.com>
2025-06-17 00:46:20 -0600

    Refactor details view to include Hero component for improved movie presentation

--- src/Views/movies/details.php
+++ src/Views/movies/details.php
@@ -1,10 +1,5 @@
 <?php
-$mainContent = function() use($title, $description, $movie) { ?>
-    <h1><?php echo $title; ?></h1>
-    <p><?php echo $description; ?></p>
-    <h2>Detalles de la Película</h2>
-    <ul>
-
-    </ul>
-<?php };
+$mainContent = function() use($title, $description, $movie) { 
+    include __DIR__ . '/../../ui/components/movies/details/Hero.php';
+};
 include __DIR__ . '/../../ui/Layouts/HomeLayout.php';
