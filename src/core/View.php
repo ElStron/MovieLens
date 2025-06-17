e8c22f7ee6082433c01b2e8aa896f412e7e753a5
ElStron <luisstron45@gmail.com>
2025-06-17 00:44:49 -0600

    Enhance View class to include HTML minification in render method

--- src/core/View.php
+++ src/core/View.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Core;
 
+require_once __DIR__ . '/../utils/minify.php';
+
 class View
 {
     public function render(string $template, array $data = []): void
@@ -13,7 +15,10 @@ class View
             http_response_code(404);
             return;
         } else {
-           require $path;
+            ob_start();
+            require $path;
+            $output = ob_get_clean();
+            echo minify_html($output);
         }
     }
 
